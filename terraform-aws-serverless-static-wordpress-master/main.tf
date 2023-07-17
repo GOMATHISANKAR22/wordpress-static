f91a70f367c947680bd95e4ab0e74a0984a70a04
GOMATHISANKAR22 <117799775+GOMATHISANKAR22@users.noreply.github.com>
2023-07-17 08:03:13 +0100

    Update main.tf

--- terraform-aws-serverless-static-wordpress-master/main.tf
+++ terraform-aws-serverless-static-wordpress-master/main.tf
@@ -7,35 +7,41 @@ module "lambda_slack" {
 }
 
 module "codebuild" {
-  source                   = "./modules/codebuild"
-  site_name                = var.site_name
- # site_domain              = var.site_domain
-  codebuild_bucket         = "${var.site_name}-build"
-  main_vpc_id              = var.main_vpc_id
-  wordpress_ecr_repository = aws_ecr_repository.serverless_wordpress.name
-  aws_account_id           = var.aws_account_id
-  container_memory         = var.ecs_memory
+  source                     = "./modules/codebuild"
+  graviton_codebuild_enabled = var.graviton_codebuild_enabled
+  site_name                  = var.site_name
+#  site_domain                = var.site_domain
+  codebuild_bucket           = "${var.site_name}-build"
+  main_vpc_id                = var.main_vpc_id
+  wordpress_ecr_repository   = aws_ecr_repository.serverless_wordpress.name
+  aws_account_id             = var.aws_account_id
+  container_memory           = var.ecs_memory
+  wp2static_version          = var.wp2static_version
+  wp2static_s3_addon_version = var.wp2static_s3_addon_version
 }
 
 module "cloudfront" {
   source             = "./modules/cloudfront"
   site_name          = var.site_name
-#  site_domain        = var.site_domain
+ # site_domain        = var.site_domain
+ # cloudfront_ssl     = aws_acm_certificate.wordpress_site.arn
   cloudfront_aliases = var.cloudfront_aliases
   providers = {
     aws.ue1 = aws.ue1
   }
-  # depends_on = [module.waf]
-  cloudfront_class = var.cloudfront_class
-  # waf_acl_arn      = var.waf_enabled ? module.waf[0].waf_acl_arn : null
+#  depends_on = [module.waf]
+
+  cloudfront_class                  = var.cloudfront_class
+#  waf_acl_arn                       = var.waf_enabled ? module.waf[0].waf_acl_arn : null
+  cloudfront_function_301_redirects = var.cloudfront_function_301_redirects
 }
 
-# module "waf" {
-#   count         = var.waf_enabled ? 1 : 0
-#   source        = "./modules/waf"
-#   site_name     = var.site_name
-#   waf_acl_rules = var.waf_acl_rules
-#   providers = {
-#     aws.ue1 = aws.ue1
-#   }
-# }
+/*module "waf" {
+  count         = var.waf_enabled ? 1 : 0
+  source        = "./modules/waf"
+  site_name     = var.site_name
+  waf_acl_rules = var.waf_acl_rules
+  providers = {
+    aws.ue1 = aws.ue1
+  }
+}*/
